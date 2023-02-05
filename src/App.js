b0470fa7c42b7a0f6fb8f15dcee14cdeed1981bf
Hemerson-Rocha <hemerson.me@gmail.com>
2023-02-05 11:02:29 -0300

    Estilizacao
    Pagina criada e estilizada

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-
+import { FiSearch } from 'react-icons/fi';
+import './style.css';
 function App() {
   return (
-    <div className="App">
-      <h1>projeto</h1>
+    <div className="container">
+      <h1 className="title">Buscar endereço pelo cep</h1>
+
+      <div className="containerInput">
+          <input type="text" placeholder="Digite seu cep..." />
+          <button className="buttonSearch"><FiSearch size={25} color="#fff"/></button>
+      </div>
+
+      <main className="main">
+        <h2>CEP: 99999999</h2>
+
+        <span>Rua chavo</span>
+        <span>complemmento: algum</span>
+        <span>vila da folha</span>
+        <span>aldeia</span>
+
+      </main>
+
     </div>
   );
 }
